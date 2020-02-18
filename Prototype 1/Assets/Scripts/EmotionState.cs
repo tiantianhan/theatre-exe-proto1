e90cb904e43a3f58d2fcb5d38fa6640fb83bb53b
NaijiaJin <43256871+NaijiaJin@users.noreply.github.com>
2020-02-18 14:02:39 -0500

    add different text scale

--- Prototype 1/Assets/Scripts/EmotionState.cs
+++ Prototype 1/Assets/Scripts/EmotionState.cs
@@ -7,11 +7,13 @@
 
 public class EmotionState
 {
+    
     public float anger = 0.0f;
     public float sadness = 0.0f;
     public float joy = 0.0f;
     public float fear = 0.0f;
-    public float envy = 0.0f;
+    public float envy = 0.0f; 
+    
 
     public void PrintState(){
         Debug.Log("State: anger " + anger + 
